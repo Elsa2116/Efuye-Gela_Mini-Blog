53a4490b4f3b50a5463b0285a23b9bb4cbaf7dda
Elsa2116m  git config --global user.name Elsa2116 <elsialemayehu332gmail.com>
2025-08-13 13:05:04 +0300

    done

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center p-4 bg-gray-100 mt-12">
-      <p>© 2025 DevConnect. Built with ❤️</p>
+      <p>© 2025 DevConnect.</p>
     </footer>
   );
 }
